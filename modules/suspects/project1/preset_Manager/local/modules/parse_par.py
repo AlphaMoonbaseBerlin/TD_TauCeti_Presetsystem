2b29beef47a54451bb5e7ffb17caa3b7c0021e18
AlphaMoonbaseBerlin <kontakt@alphamoonbase.de>
2023-08-03 10:13:49 +0200

    added parser fr OP-Style parameters to return val instead of evaluated (which resulted in path)

--- modules/suspects/project1/preset_Manager/local/modules/parse_par.py
+++ modules/suspects/project1/preset_Manager/local/modules/parse_par.py
@@ -2,8 +2,8 @@
 Name : parse_par
 Author : Wieland@AMB-ZEPH15
 Version : 0
-Build : 2
-Savetimestamp : 2023-07-26T10:59:55.612727
+Build : 3
+Savetimestamp : 2023-08-03T10:13:11.598178
 Saveorigin : TauCetiV4.toe
 Saveversion : 2022.32660
 Info Header End'''
@@ -15,8 +15,19 @@ def bool_parse( parameter ):
 def null_parse( parameter ):
 	return parameter.eval()
 
+def val_parse( parameter ):
+	return parameter.val
+
 parser = { 		"Toggle" : bool_parse,
-	  		"Momentary" : bool_parse }
+	  		"Momentary" : bool_parse,
+			"OP"		: val_parse,
+			"COMP"		: val_parse,
+			"TOP"		: val_parse,
+			"DAT"		: val_parse,
+			"CHOP"		: val_parse,
+			"MAT"		: val_parse,
+			"SOP"		: val_parse
+			 }
 	
 def parse( parameter ):
 	return parser.get( parameter.style, null_parse )( parameter )
